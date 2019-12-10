b1141385cf3c8f8b12f41b1a4b309a2dc970a875
Stanley Lee <sachih@live.com>
2019-12-10 00:19:12 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -46,7 +46,7 @@ def total_gross(source)
   # Visit each key (i.e. director name), look up the value in the hash
   # returned by directors_totals, and add it to a running total. When done,
   # return the total
-total_earnings = directors_total(source)
+total_earnings = directors_totals(source)
 directors_names = list_of_directors(source)
 
 total = 0
